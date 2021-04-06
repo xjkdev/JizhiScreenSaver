4b00118c461a8bfdaea681f845cbb71cde542721
xjkdev <xjk2008@gmail.com>
2021-04-06 13:31:29 +0800

    Initial Commit

---
+++ jizhi_saver/jizhi_saverView.h
@@ -0,0 +1,12 @@
+//
+//  jizhi_saverView.h
+//  jizhi_saver
+//
+//  Created by 谢俊琨 on 2021/4/6.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface jizhi_saverView : ScreenSaverView
+
+@end
---
+++ jizhi_saver/jizhi_saverView.m
@@ -0,0 +1,51 @@
+//
+//  jizhi_saverView.m
+//  jizhi_saver
+//
+//  Created by 谢俊琨 on 2021/4/6.
+//
+
+#import "jizhi_saverView.h"
+
+@implementation jizhi_saverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
